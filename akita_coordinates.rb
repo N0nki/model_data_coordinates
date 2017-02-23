04f95ba3ae5aa0f368b27625ffb36991caa4bd77
N0nki <rgstr_land@icloud.com>
2017-02-24 05:16:33 +0900

    fix method name

--- akita_coordinates.rb
+++ akita_coordinates.rb
@@ -3,5 +3,5 @@ require_relative "get_coordinates"
 akita_cities = %w(鹿角 大館 北秋田 能代 秋田 男鹿 潟上 由利本荘 にかほ 大仙 仙北 横手 湯沢)
 # get_coodinates(akita_cities, "akita_cities_coodinates2.csv",
 #                language: :ja, http_proxy: "wproxy.akita-u.ac.jp:8080", prefix: "秋田県")
-get_coodinates(akita_cities, "akita_cities_coordinates.csv",
+get_coordinates(akita_cities, "akita_cities_coordinates.csv",
                language: :ja, prefix: "秋田県")
