dfa679b71b5f20568b9ef291065355cf66dbafeb
N0nki <rgstr_land@icloud.com>
2017-02-24 05:03:59 +0900

    fix method name

--- cost239_coordinates.rb
+++ cost239_coordinates.rb
@@ -1,5 +1,6 @@
-require "./get_coodinates"
+require_relative "get_coordinates"
 
 cities = %w(Copenhagen London Amsterdam Berlin Bruxelles Luxembourg Prague
             Paris Zurich Vienne Milan)
-get_coodinates(cities, "cost239_coordinates.csv", http_proxy: "wproxy.akita-u.ac.jp:8080")
+# get_coordinates(cities, "cost239_coordinates.csv", http_proxy: "wproxy.akita-u.ac.jp:8080")
+get_coordinates(cities, "cost239_coordinates.csv")
